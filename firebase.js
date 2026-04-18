041eebdd2a49b564edd26d8c5c3431a54d5ae33a
Diego Cardona <diego.cardona06@usc.edu.co>
2026-04-18 02:37:10 -0500

    Ajuste de firebase

--- firebase.js
+++ firebase.js
@@ -1,13 +1,16 @@
+// CONFIG REAL (la tuya)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyCavRPIHaJ4R_jScWoPRyv8pEuoPIonvP0",
   authDomain: "diagnostico-pymes-2e83d.firebaseapp.com",
   projectId: "diagnostico-pymes-2e83d",
   storageBucket: "diagnostico-pymes-2e83d.appspot.com",
   messagingSenderId: "918351696404",
-  appId: "1:918351696404:web:xxxx"
+  appId: "1:918351696404:web:deb958cbe4351e51968d01"
 };
 
+// INICIALIZAR (FORMA V8)
 firebase.initializeApp(firebaseConfig);
 
+// 👇 ESTO ES LO IMPORTANTE
 const auth = firebase.auth();
 const db = firebase.firestore();
